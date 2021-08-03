2d3a7f89477a14cefb48d3e914131b267f92f3fc
Jeremy Westhead <jeremywesthead3@gmail.com>
2021-08-03 16:53:46 +0100

    Added CSS + made mutations easier to view

--- README.md
+++ README.md
@@ -1,3 +1,26 @@
 # Protein-Mutation-Viewer
 A flask server to allow visualisation of protein mutations.
 Uses a customised version of Mol* to provide a viewer.
+
+## Installation
+```
+git clone --recurse-submodules git@github.com:JeremyWesthead/Protein-Mutation-Viewer.git
+cd Protein-Mutation-Viewer
+
+#Build the viewer
+cd molstar-mutation
+npm install
+npm run build
+
+#Install requirements for flask
+cd ..
+python -m virtualenv env
+source env/bin/activate
+pip install -r requirements.txt
+```
+
+## Run
+This will start the webserver on port 4000
+```
+python app.py
+```
--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, redirect
 import os.path
 
 
@@ -11,7 +11,16 @@ REFERENCE_DIR = "data/reference-proteins/"
 MUTATIONS_DIR = "data/mutations/"
 DATASET = pd.read_pickle("data/MUTATIONS-Spike.pkl.gz")
 
-def get_muatation_counts(mutations):
+def get_muatation_counts(mutations, threshold=5):
+    '''Function to get the normalised number of mutations at each amino acid residue
+
+    Args:
+        mutations (list): List of mutations in GARC
+        threshold (int, optional): Threshold to consider a mutation. Defaults to 5
+
+    Returns:
+        dict: Dictionary mapping mutation_index->normalised_count
+    '''    
     assert len(mutations) > 0
     mutation_counts = defaultdict(int)
     #Checking for SNPs first
@@ -25,12 +34,19 @@ def get_muatation_counts(mutations):
     max_count = max(mutation_counts.values())
 
     def normalise(count, max_count):
-        return round(0.75* (count / max_count)**2 + 0.25, 2)
+            return round(0.75* (count / max_count)**2 + 0.25, 2)
 
-    mutation_counts = {index: normalise(mutation_counts[index], max_count) for index in mutation_counts.keys()}
+    mutation_counts = {index: normalise(mutation_counts[index], max_count) for index in mutation_counts.keys() if mutation_counts[index] >= threshold}
     return mutation_counts
 
 def write_new_pdb(mutation_counts, filename="out.pdb", reference=REFERENCE_DIR+"6vxx.pdb"):
+    '''Writes a new pdb file with the occupancy field altered appropriately for the mutation_counts
+
+    Args:
+        mutation_counts (dict): Dictionary mapping mutation_index->normalised_count
+        filename (str, optional): Output filename for the resultant pdb file. Defaults to "out.pdb".
+        reference (str, optional): Filename of the reference pdb file. Defaults to REFERENCE_DIR+"6vxx.pdb".
+    '''    
     filename = MUTATIONS_DIR+filename
     #Read the reference pdb file
     with open(reference) as f:
@@ -60,19 +76,65 @@ def write_new_pdb(mutation_counts, filename="out.pdb", reference=REFERENCE_DIR+"
                     line[56:60] = list(str(mutation_counts[int(trim(''.join(line[22:26])))]))
                 else:
                     #Default to 0 if there were no mutations
+                    continue
                     line[56:60] = list("0.00")
                 line = ''.join(line)
             f.write(line)
 
+
+def write_reference_pdb(filename="out.pdb", reference=REFERENCE_DIR+"6vxx.pdb"):
+    '''Writes a new pdb file with the occupancy field altered to be 0 consistently - 
+    creating a blank cartoon representation
+
+    Args:
+        filename (str, optional): Output filename for the resultant pdb file. Defaults to "out.pdb".
+        reference (str, optional): Filename of the reference pdb file. Defaults to REFERENCE_DIR+"6vxx.pdb".
+    '''    
+    filename = REFERENCE_DIR+filename
+    #Read the reference pdb file
+    with open(reference) as f:
+        ref = [line for line in f]
+
+    def trim(s):
+        '''Takes in a string and returns it without trailing or leading whitespace
+
+        Args:
+            s (str): String
+        '''
+        if s[0] == " ":
+            return trim(s[1:])
+        if s[-1] == " ":
+            return trim(s[:-1])
+        return s    
+
+    #Write new file
+    with open(filename, "w") as f:
+        for line in ref:
+            if line[:5] == "ATOM ":
+                line = list(line)
+                line[56:60] = list("0.00")
+                line = ''.join(line)
+            f.write(line)
+
 def run():
+    '''Factory method for the Flask server
+    '''    
     app = Flask(__name__, static_url_path='', static_folder='', template_folder='templates')
 
     @app.route("/")
     def index():
+        '''Render the index page
+        '''  
+        print(render_template("index.html"))      
         return render_template("index.html")
     
-    @app.route("/viewer/pango/<lineage>")
+    @app.route("/viewer/covid/spike/pango/<lineage>")
     def view_lineage(lineage):
+        '''Page for viewing mutations in the covid spike protein
+
+        Args:
+            lineage (str): Name of the pango lineage
+        '''        
         #Check if the pdb has already been made, and generate as required
         if not os.path.isfile(MUTATIONS_DIR+lineage+".pdb"):
             try:
@@ -80,10 +142,15 @@ def run():
             except AssertionError:
                 #This mutation has no data
                 return "<h3>No data found for lineage "+lineage+"</h3>"
+        if not os.path.isfile(REFERENCE_DIR+"6vxx-blank.pdb"):
+            write_reference_pdb(filename="6vxx-blank.pdb")
+        print(render_template("viewer.html", pdb=lineage))
         return render_template("viewer.html", pdb=lineage)
     
     @app.route("/list/pango")
     def list_lineages():
+        '''Render a page containing a list of pango lineages
+        '''        
         lineages = sorted(list(set(DATASET["pango_lineage"])))
         return render_template("pango_list.html", lineages=lineages)
     
